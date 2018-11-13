d47a0a2215ee13507dc5073c36f5a4db7712ec4e
Rahul Patchigolla <rapatchi@microsoft.com>
2018-11-13 06:23:28 +0000

    mvn cli commit #7

--- sf-mesh-maven-plugin/src/main/java/com/microsoft/azure/maven/servicefabric/AddSecretValueMojo.java
+++ sf-mesh-maven-plugin/src/main/java/com/microsoft/azure/maven/servicefabric/AddSecretValueMojo.java
@@ -51,8 +51,8 @@ public class AddSecretValueMojo extends AbstractMojo
         	throw new MojoFailureException("Service fabric resources folder does not exist. Please run init goal before running this goal!");
         }
         else{
-            String[] secretValueSplit = secretValueName.split("\\", 2);
-            if(Utils.checkIfExists(Utils.getPath(appResourcesDirectory, "secretvalue_" + secretValueSplit[0] + ".yaml"))){
+            String[] secretValueSplit = secretValueName.split("/", 2);
+            if(Utils.checkIfExists(Utils.getPath(appResourcesDirectory, "secretvalue_" + secretValueSplit[0] +"_"+ secretValueSplit[1]+ ".yaml"))){
                 throw new MojoFailureException("Secret Value Resource with the specified name already exists");
             }
             InputStream resource = this.getClass().getClassLoader().getResourceAsStream(Constants.SECRET_VALUE_RESOURCE_NAME);
@@ -61,7 +61,7 @@ public class AddSecretValueMojo extends AbstractMojo
                 secretValueContent = Utils.replaceString(logger, secretValueContent, "SCHEMA_VERSION", schemaVersion, Constants.SECRET_VALUE_RESOURCE_NAME);
                 secretValueContent = Utils.replaceString(logger, secretValueContent, "SECRET_VALUE_NAME", secretValueName, Constants.SECRET_VALUE_RESOURCE_NAME);
                 secretValueContent = Utils.replaceString(logger, secretValueContent, "SECRET_VALUE", secretValue, Constants.SECRET_VALUE_RESOURCE_NAME);
-                FileUtils.fileWrite(Utils.getPath(appResourcesDirectory, "secretvalue_" + secretValueName + ".yaml"), secretValueContent);
+                FileUtils.fileWrite(Utils.getPath(appResourcesDirectory, "secretvalue_" + secretValueSplit[0] +"_"+ secretValueSplit[1]+ ".yaml"), secretValueContent);
 				logger.debug(String.format("Wrote %s secret value content to output", secretValueName));
                 TelemetryHelper.sendEvent(TelemetryEventType.ADDSECRETVALUE, String.format("Added secret value with name: %s", secretValueName), logger);
             }
